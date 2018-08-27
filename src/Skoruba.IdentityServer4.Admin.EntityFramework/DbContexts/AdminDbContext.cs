5f2135d8702b5b2afcf7fbdedb2e4261cad3b251
Alex Tron <alex.tron@trov.com>
2018-08-27 17:13:34 +0300

    Imported from IdentityServer4.Admin

---
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework/DbContexts/AdminDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Constants;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Entities;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Entities.Identity;
+
+namespace Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts
+{
+    public class AdminDbContext : IdentityDbContext<UserIdentity, UserIdentityRole, int, UserIdentityUserClaim, UserIdentityUserRole, UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken>
+    {
+        public AdminDbContext(DbContextOptions<AdminDbContext> options)
+            : base(options)
+        {
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            ConfigureIdentityContext(builder);  
+        }
+
+        private void ConfigureIdentityContext(ModelBuilder builder)
+        {
+            builder.Entity<UserIdentityRole>().ToTable(TableConsts.IdentityRoles);
+            builder.Entity<UserIdentityRoleClaim>().ToTable(TableConsts.IdentityRoleClaims);
+            builder.Entity<UserIdentityUserRole>().ToTable(TableConsts.IdentityUserRoles);
+
+            builder.Entity<UserIdentity>().ToTable(TableConsts.IdentityUsers);
+            builder.Entity<UserIdentityUserLogin>().ToTable(TableConsts.IdentityUserLogins);
+            builder.Entity<UserIdentityUserClaim>().ToTable(TableConsts.IdentityUserClaims);
+            builder.Entity<UserIdentityUserToken>().ToTable(TableConsts.IdentityUserTokens);
+        }
+    }
+}
---
+++ src/Skoruba.IdentityServer4.AspNetIdentity/Startup.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Entities.Identity;
+
+namespace Skoruba.IdentityServer4.AspNetIdentity
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (environment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionStringIdentity = Configuration.GetConnectionString("AdminConnectionIdentity");
+            string connectionStringConfiguration = Configuration.GetConnectionString("AdminConnectionConfiguration");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<AdminDbContext>(options =>
+                options.UseMySql(connectionStringIdentity, mysqlOptions =>
+                    {
+                        mysqlOptions.ServerVersion(new Version(5, 7), ServerType.MySql);
+                    }));
+
+            services.AddDbContext<ConfigurationDbContext>(options =>
+                options.UseMySql(connectionStringConfiguration, mysqlOptions =>
+                {
+                    mysqlOptions.ServerVersion(new Version(5, 7), ServerType.MySql);
+                }));
+
+            services.AddIdentity<UserIdentity, UserIdentityRole>()
+                .AddEntityFrameworkStores<AdminDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            //string connectionStringConfiguration = Configuration.GetConnectionString("AdminConnectionConfiguration");
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddAspNetIdentity<UserIdentity>()
+                // this adds the config data from DB (clients, resources)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseMySql(connectionStringConfiguration,
+                            sql =>
+                            {
+                                sql.MigrationsAssembly(migrationsAssembly);
+                                sql.ServerVersion(new Version(5, 7), ServerType.MySql);
+                            });
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseMySql(connectionStringConfiguration,
+                            sql =>
+                            {
+                                sql.MigrationsAssembly(migrationsAssembly);
+                                sql.ServerVersion(new Version(5, 7), ServerType.MySql);
+                            });
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+
+#if DEBUG
+                    options.TokenCleanupInterval = 15; // frequency in seconds to cleanup stale grants. 15 is useful during debugging
+#endif                
+                });
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
